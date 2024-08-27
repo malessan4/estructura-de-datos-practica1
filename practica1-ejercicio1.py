1ae0eadd574fbc99664ab0465f219ebcb9c2ed33
Matias Alessandrello <matialess@gmail.com>
2024-08-27 19:20:14 -0300

    practica1-ejercicio1

---
+++ practica1-ejercicio1.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio 1: Dada la siguiente información,
+elija una estructura de datos que permita guardarla adecuadamente.
+"""
+
+"""
+Guerra del Peloponeso 431 a.C
+Revolución de Mayo 1810 d.C
+Llegada de los españoles a América 1492 d.C
+Comienzo de la construcción de la gran Muralla China 214 a.C
+"""
+
+hechos_historicos = { 1: "Comienzo de la construcción de la gran Muralla China 214 a.C", 2: "Guerra del Peloponeso 431 a.C", 3: "Llegada de los españoles a América 1492 d.C", 4: "Revolución de Mayo 1810 d.C" }
+
+for values in hechos_historicos.values():
+    print (values)
